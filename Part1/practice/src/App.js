3983ef94e65a5fd77f7c1754987193569d783e5f
Scott-Zeta <wc070211@gmail.com>
2022-07-31 15:52:15 +0930

    divide to 3 componets

--- Part1/practice/src/App.js
+++ Part1/practice/src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
 import { useState } from 'react'
 
+const Display = ({counter,time}) =>{
+  return (
+    <div>{counter} {time}</div>
+  )
+}
+
+const Button = ({onClick,text}) => (
+  <button onClick = {onClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
   const [counter, setCounter] = useState(0)
   const time = new Date();
@@ -10,22 +22,15 @@ const App = () => {
   // )
   const add = () => setCounter(counter + 1)
   const minus = () => setCounter(counter - 1)
-
+  const reset = () => setCounter(0)
   console.log('rendering...', counter)
 
   return (
     <div>
-      {counter}
-      <button onClick={add}>
-        Plus
-      </button>
-      <button onClick={minus}>
-        Minus
-      </button>
-      <button onClick={() => setCounter(0)}>
-        Reset
-      </button>
-      {time.toString()}
+      <Display counter = {counter} time = {time.toString()}/>
+      <Button text = "Plus" onClick = {add}/>
+      <Button text = "Minus" onClick = {minus}/>
+      <Button text = "Reset" onClick = {reset}/>
     </div>
   )
 }
