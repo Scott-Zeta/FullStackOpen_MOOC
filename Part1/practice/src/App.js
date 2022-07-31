abc69ed8c1c709b7cf8cf004d0bd4ae682f9ed73
Scott-Zeta <wc070211@gmail.com>
2022-07-31 13:51:10 +0930

    count in useState

--- Part1/practice/src/App.js
+++ Part1/practice/src/App.js
@@ -1,56 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <p>Hello</p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-const Knock = (props) => {
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+  const time = new Date();
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+  
   return (
     <div>
-      <p>knock {props.name}, at {props.address}</p>
+      {counter}
+      {time.toString()}
     </div>
-  )
-}
-
-const App = (props) => {
-  console.log("Hello from component APP")
-  const now = new Date();
-  const a = 20;
-  const b = 60;
-  const {counter} = props
-  return (
-    <>
-      <p>
-        Hello, it is {now.toString()}
-      </p>
-      <p>
-        {counter}
-      </p>
-      <p>
-        {a} + {b} equal {a + b}
-      </p>
-      <Knock name = "King DDD" address = "Adelaide Uni"/>
-      <Knock name = "Kirby" address = {a+b} />
-      <Knock />
-    </>
   )
 }
 
--- Part1/practice/src/index.js
+++ Part1/practice/src/index.js
@@ -4,25 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-let counter = 1;
 
-const refresh = () => {
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <App counter={counter} />
-  )
-}
-
-setInterval(() => {
-  refresh()
-  counter += 1
-}, 1000)
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App counter={counter} />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
