1fc0e540616aabdcbec196d1f5fee977045f9d9d
Scott-Zeta <wc070211@gmail.com>
2022-07-31 16:50:50 +0930

    coordinate controller

--- Part1/practice/src/App.js
+++ Part1/practice/src/App.js
@@ -1,36 +1,88 @@
 import './App.css';
 import { useState } from 'react'
 
-const Display = ({counter,time}) =>{
+const Display = ({ counter }) => {
   return (
-    <div>{counter} {time}</div>
+    <>{counter}</>
   )
 }
 
-const Button = ({onClick,text}) => (
-  <button onClick = {onClick}>
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
     {text}
   </button>
 )
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState({ left: 0, right: 0, up: 0, down: 0 })
   const time = new Date();
   // setTimeout(
   //   () => setCounter(counter + 1),
   //   1000
   // )
-  const add = () => setCounter(counter + 1)
-  const minus = () => setCounter(counter - 1)
-  const reset = () => setCounter(0)
-  console.log('rendering...', counter)
+  const moveLeft = () => {
+    const newCounter = {
+      ...counter,
+      left: counter.left + 1,
+      right: counter.right - 1
+    }
+    setCounter(newCounter)
+  }
+  const moveRight = () => {
+    const newCounter = {
+      ...counter,
+      left: counter.left - 1,
+      right: counter.right + 1
+    }
+    setCounter(newCounter)
+  }
+  const moveUp = () => {
+    const newCounter = {
+      ...counter,
+      up: counter.up + 1,
+      down: counter.down - 1
+    }
+    setCounter(newCounter)
+  }
+  const moveDown = () => {
+    const newCounter = {
+      ...counter,
+      up: counter.up - 1,
+      down: counter.down + 1
+    }
+    setCounter(newCounter)
+  }
+  const reset = () => {
+    const newCounter = {
+      up: 0,
+      down: 0,
+      left: 0,
+      right: 0
+    }
+    setCounter(newCounter)
+  }
 
   return (
     <div>
-      <Display counter = {counter} time = {time.toString()}/>
-      <Button text = "Plus" onClick = {add}/>
-      <Button text = "Minus" onClick = {minus}/>
-      <Button text = "Reset" onClick = {reset}/>
+      <div>
+        <Display counter={counter.up} />
+      </div>
+      <div>
+        <Button text="UP" onClick={moveUp} />
+      </div>
+      <div>
+        <Display counter={counter.left} />
+        <Button text="Left" onClick={moveLeft} />
+        <Button text="Reset" onClick={reset} />
+        <Button text="Right" onClick={moveRight} />
+        <Display counter={counter.right} />
+      </div>
+      <div>
+        <Button text="Down" onClick={moveDown} />
+      </div>
+      <div>
+        <Display counter={counter.down} />
+      </div>
     </div>
   )
 }
