71568a8cfc040419b6b6b3a0c490e589a9927ce8
Scott-Zeta <wc070211@gmail.com>
2022-07-31 15:12:51 +0930

    click handle add or minus counter

--- Part1/practice/src/App.js
+++ Part1/practice/src/App.js
@@ -2,18 +2,29 @@ import './App.css';
 import { useState } from 'react'
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [counter, setCounter] = useState(0)
   const time = new Date();
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-  
+  // setTimeout(
+  //   () => setCounter(counter + 1),
+  //   1000
+  // )
+  const add = () => setCounter(counter + 1)
+  const minus = () => setCounter(counter - 1)
+  const handleClick = () => {
+    console.log('clicked')
+  }
+
   console.log('rendering...', counter)
 
   return (
     <div>
       {counter}
+      <button onClick={add}>
+        plus
+      </button>
+      <button onClick={minus}>
+        minus
+      </button>
       {time.toString()}
     </div>
   )
