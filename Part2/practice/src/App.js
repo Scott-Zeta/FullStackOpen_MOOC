cbae5f447fc19f0cdb5f95d125cfa677ad0f7ec2
Scott-Zeta <wc070211@gmail.com>
2022-08-15 12:47:51 +0930

    basic reduce function

--- Part2/practice/src/App.js
+++ Part2/practice/src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const animals = [{ name: "A", species: "dog" },
-  { name: "B", species: "cat" },
-  { name: "C", species: "cat" },
-  { name: "D", species: "dog" },
-  { name: "E", species: "fish" }
+  const animals = [
+    { name: "A", species: "dog", weight: 5 },
+    { name: "B", species: "cat", weight: 1 },
+    { name: "C", species: "cat", weight: 2 },
+    { name: "D", species: "dog", weight: 3 },
+    { name: "E", species: "fish", weight: 4 }
   ];
 
   // const isDog = function (animal) {
@@ -18,10 +19,13 @@ function App() {
   });
   console.log(dog);
 
-  const names = animals.map(function (animals){
+  const names = animals.map(function (animals) {
     return animals.name;
   })
   console.log(names);
+
+  let totalWeight = animals.reduce((sum, animal) => sum + animal.weight, 0);
+  console.log(totalWeight);
 
   return (
     <div className="App">
