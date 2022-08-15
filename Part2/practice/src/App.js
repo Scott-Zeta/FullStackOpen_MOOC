939e312db73fd388cc09392ad8d36345cbc3975f
Scott-Zeta <wc070211@gmail.com>
2022-08-15 11:56:55 +0930

    functional programming filter

--- Part2/practice/src/App.js
+++ Part2/practice/src/App.js
@@ -2,6 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const animals = [{ name: "A", species: "dog" },
+  { name: "B", species: "cat" },
+  { name: "C", species: "cat" },
+  { name: "D", species: "dog" },
+  { name: "E", species: "fish" }
+  ];
+
+  const isDog = function (animal) {
+    return animal.species === 'dog'
+  }
+
+  const dog = animals.filter(isDog);
+
+  //console.log(isDog);
+  console.log(dog);
+
   return (
     <div className="App">
       <header className="App-header">
