a449fd906277d8548d34ef25d233b22f16385278
Scott-Zeta <wc070211@gmail.com>
2022-09-12 19:20:07 +0930

    split component, add key for render

--- Part2/phonebook/src/components/Content.js
+++ Part2/phonebook/src/components/Content.js
@@ -1,4 +1,13 @@
+import React from 'react'
+import Person from './Person';
+
 const Content = ({persons}) =>{
 console.log(persons.length)
+
+return(
+    <ul>
+        {persons.map((p,i) => <Person key={i} person={p}/>)}
+    </ul>
+)
 }
 export default Content
