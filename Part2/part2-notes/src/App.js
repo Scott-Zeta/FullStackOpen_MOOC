e468051d85d506ff4534d70ff31faa7049a375b4
Scott-Zeta <wc070211@gmail.com>
2022-09-22 16:14:42 +0930

    refactored

--- Part2/part2-notes/src/App.js
+++ Part2/part2-notes/src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -8,15 +9,19 @@ const App = () => {
   const [showAll, setShowAll] = useState(true)
 
   const hook = () => {
-    console.log('effect')
+    // console.log('effect')
 
-    const evenHandler = response => {
-      console.log('promise fulfilled')
-      setNotes(response.data)
-    }
-    axios
-      .get('http://localhost:3001/notes')
-      .then(evenHandler)
+    // const eventHandler = response => {
+    //   console.log('promise fulfilled')
+    //   setNotes(response.data)
+    // }
+    // axios                                   //These two parts
+    //   .get('http://localhost:3001/notes')    // was refactored
+    //   .then(eventHandler)
+    noteService.getAll()
+      .then(response => {
+        setNotes(response.data)
+      })
   } //get notes from the server
 
   useEffect(hook, [])
@@ -35,15 +40,19 @@ const App = () => {
       id: notes.length + 1,
     }
 
-    axios.post('http://localhost:3001/notes', noteObject)
-      .then(response => {
-        console.log(response)
-        setNotes(notes.concat(response.data)) // concat the new note behind the old notes
-        setNewNote("")//clear the new note builder, clean the input form after submmit
-      }) //the notes won't disappear when refreshing
+    // axios.post('http://localhost:3001/notes', noteObject)
+    //   .then(response => {
+    //     console.log(response)
+    //     setNotes(notes.concat(response.data)) // concat the new note behind the old notes
+    //     setNewNote("")//clear the new note builder, clean the input form after submmit
+    //   }) //the notes won't disappear when refreshing
 
     //console.log(noteObject, event.target) //event.target is the target the event has been excuted on
-
+    noteService.create(noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
   }
 
   const handleNoteChange = (event) => { //handle the change in the input box
@@ -58,10 +67,14 @@ const App = () => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
 
-    axios.put(url, changedNote).then(response => {
-      setNotes(notes.map(n => n.id !== id ? n : response.data))//use map to subsitute the old notes, 
-      //if id not same, assign to itself(skip), if it is, assign to new notes
-    })
+    // axios.put(url, changedNote).then(response => {
+    //   setNotes(notes.map(n => n.id !== id ? n : response.data))//use map to subsitute the old notes, 
+    //   //if id not same, assign to itself(skip), if it is, assign to new notes
+    // })
+    noteService.update(id, changedNote)
+      .then(response => {
+        setNotes(notes.map(note => note.id !== id ? note : response.data))
+      })
   } ///!! important
 
   return (
