a935d98a8da7257495c5e74542865543a34d56bb
Scott-Zeta <wc070211@gmail.com>
2022-09-28 17:11:48 +0930

    POST method with condition

--- Part3/practice/index.js
+++ Part3/practice/index.js
@@ -50,11 +50,28 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
+const getId = () =>{
+    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0 
+    //get current id, three dots "..." to make map array to individual number. this is really fucking horrible
+    return maxId + 1
+}
+
 app.post('/api/notes', (request, response) => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0 //get current id, this is really fucking horrible
-    const note = request.body
-    note.id = maxId + 1
+    const body = request.body
     
+    if(!body.content){
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note ={
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: getId(), 
+    }
+
     notes = notes.concat(note)
 
     response.json(note)
